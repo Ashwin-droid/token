5545efa524641de974fcd22c221b004e6d4177e3
Ashwin-droid <58349846+Ashwin-droid@users.noreply.github.com>
2022-06-03 10:37:16 +0000

    fixed all reeors from basic barebones

--- src/token_assets/src/index.jsx
+++ src/token_assets/src/index.jsx
@@ -1,9 +1,10 @@
-import ReactDOM from 'react-dom'
+import { createRoot } from 'react-dom/client';
 import React from 'react'
 import App from "./components/App";
 
 const init = async () => { 
-  ReactDOM.render(<App />, document.getElementById("root"));
+  const root = createRoot(document.getElementById('root'));
+  root.render(<App />);
 }
 
 init();
